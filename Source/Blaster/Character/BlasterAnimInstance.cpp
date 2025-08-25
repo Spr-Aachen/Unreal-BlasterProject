07f56705528b5a33337a44849738013ea59c707e
Spr_Aachen <2835946988@qq.com>
2025-08-25 14:58:13 +0800

    Add key variables to drive basic unequipped animations

--- Source/Blaster/Character/BlasterAnimInstance.cpp
+++ Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -1,5 +1,41 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "GameFramework/CharacterMovementComponent.h"
 
 #include "BlasterAnimInstance.h"
+#include "BlasterCharacter.h"
 
+
+void UBlasterAnimInstance::NativeInitializeAnimation(){
+    Super::NativeInitializeAnimation();
+
+    // Cast to character
+    BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+}
+
+
+void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime){
+    Super::NativeUpdateAnimation(DeltaTime);
+
+    /* Cast to character in the first frame */
+    if (BlasterCharacter == nullptr){
+        BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+    }
+    if (BlasterCharacter == nullptr){
+        return;
+    }
+
+    /* Set speed */
+    // Get velocity
+    FVector Velocity = BlasterCharacter->GetVelocity();
+    // Zero out Z velocity
+    Velocity.Z = 0.f;
+    // Get speed through size of the vector
+    Speed = Velocity.Size();
+
+    /* Set bIsInAir */
+    bIsInAir = BlasterCharacter->GetCharacterMovement()->IsFalling();
+
+    /* Get bIsAccelerating */
+    bIsAccelerating = BlasterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false; // Using ternary operator
+}
--- Source/Blaster/Character/BlasterAnimInstance.h
+++ Source/Blaster/Character/BlasterAnimInstance.h
@@ -4,14 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+
 #include "BlasterAnimInstance.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
-class BLASTER_API UBlasterAnimInstance : public UAnimInstance
-{
+class BLASTER_API UBlasterAnimInstance : public UAnimInstance{
 	GENERATED_BODY()
-	
+
+public:
+	// Called at the very beginning of the game (much like BeginPlay function)
+	virtual void NativeInitializeAnimation() override;
+	// Called every frame (much like Tick function)
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+private:
+	/* Variables to expose to animation bluprint */
+	// Character
+	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true")) // Use this meta specifier to allow private access when using BlueprintReadOnly for private member
+	class ABlasterCharacter *BlasterCharacter; // Foward declare the character pointer that is going to use the animation instance
+	// Speed
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true")) // Use this meta specifier to allow private access when using BlueprintReadOnly for private member
+	float Speed;
+	// IsInAir
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true")) // Use this meta specifier to allow private access when using BlueprintReadOnly for private member
+	bool bIsInAir;
+	// IsAccelerating
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true")) // Use this meta specifier to allow private access when using BlueprintReadOnly for private member
+	bool bIsAccelerating;
 };
